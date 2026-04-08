731608ada10fd90fd87994dde66c942113455d49
susmi. learn <susmi.learn@gmail.com>
2026-04-08 10:33:05 +0530

    comments added

--- js/index.js
+++ js/index.js
@@ -1,59 +1,6 @@
-// // text area
-// let displayInput = document.getElementById("show-text");
-
-// // get all clicked button elements in text area
-// let buttons = document.querySelectorAll("button");
-// buttons.forEach(button => {
-//   button.addEventListener('click', (event) => {
-//     displayInput.value += event.target.innerText;
-//   });
-// });
-
-// // submit button click event to show result
-// let submitButton = document.getElementById("submit")
-// submitButton.addEventListener('click', () => {
-//   let output = displayInput.value;
-//   let expression = output.replace('=', '')
-//     .replace(/x²/g, '**2')
-//     .replace(/x³/g, '**3')
-//     .trim();
-//   let solution = eval(expression);
-//   displayInput.value = solution;
-// });
-
-// // clear button event
-// let clearButton = document.getElementById("clearbtn")
-// clearButton.addEventListener('click', () => {
-//   displayInput.value = "";
-// });
-
-// // square value
-// let squareButton = document.getElementById("squarebtn");
-// squareButton.addEventListener("click", () => {
-//   let square = displayInput.value;
-//   if (square !== "") {
-//     let num = Number(square);
-
-//     if (!isNaN(num)) {
-//       displayInput.value = num ** 2;
-//     }
-//   }
-// });
-
-// // cube value
-// let cubeButton = document.getElementById("cubebtn");
-// cubeButton.addEventListener("click", () => {
-//   let cube = displayInput.value;
-//   if (cube !== "") {
-//     let num = Number(cube);
-
-//     if (!isNaN(num)) {
-//       displayInput.value = num ** 3;
-//     }
-//   }
-// });
-
+// get text area
 const display = document.getElementById("show-text");
+// get all button
 const button = document.querySelectorAll("button");
 
 let num1 = "";
@@ -63,7 +10,7 @@ let operator = "";
 button.forEach(btn => {
   btn.addEventListener("click", () => {
     let value = btn.innerText;
-
+    
     if (value === "C") {
       display.value = "";
       num1 = num2 = operator = "";
